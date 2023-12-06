64ce364fcef359f7fc9189513f08d16eee24f0ec
Tathya551 <tathya551@gmail.com>
2023-12-07 04:32:13 +0530

    Added my details

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
-// import MyDetails from "./MyDetails";
+import MyDetails from "./MyDetails";
 import ResumeLink from "./ResumeLink";
 
 const LandingPage = () => (
   <div>
-    <h1>Welcome</h1>
-    {/* <MyDetails /> */}
+    <h1>Hey CubixCraft team</h1>
+    <MyDetails />
     <ResumeLink />
     <Link to="/bmi-calculator">BMI Calculator</Link>
   </div>
---
+++ src/components/MyDetails.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const MyDetails = () => {
+  return (
+    <div>
+      <p>Hello! I'm Tathya.</p>
+      <p>
+        I am a graduate in B.E. (Computer engineering), I completed my degree in
+        June 2022.
+      </p>
+      <p>
+        I have worked in MERN stack but my main expertise lies in frontend web
+        development with a working proficiency in ReactJS and a good
+        understanding of NextJS. I also possess a good grasp over backend
+        development and I have knowledge of NodeJS, ExpressJS and MongoDB.
+        Although I primarily code in Javascript, I am also proficient in C,
+        Java, and Python. I have the ability to solve problems and write and
+        analyse code. I have a good understanding of mySQL and I have also
+        worked with it. I am also familiar with noSQL, particularly mongoDB as a
+        part of backend development.
+      </p>
+    </div>
+  );
+};
+
+export default MyDetails;
